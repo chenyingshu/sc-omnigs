62ec0016a0b2221f4984e7621d3db7d0c06d4f8c
SusanYingshuChen <yingshu2008@gmail.com>
2024-05-27 16:41:43 +0800

    slider

--- js/main.js
+++ js/main.js
@@ -14,12 +14,24 @@ $(document).ready(function()  {
 
     carousel_comp_item_idx = 0;
     _carousel_comp_prefix = 'compare-btn-';
+    _carousel_comp_item_prefix = 'compare-res';
     CarouselCompDots = [];
-    compare_number = 5;
+    compare_number = 7;
+    DicsLists = [];
+    DicsItems = [];
     for (let i = 0; i < compare_number; i++) {
         CarouselCompDots.push(document.getElementById(_carousel_comp_prefix + i.toString()));
         CarouselCompDots[i].addEventListener('click', _change_active_comp_dot.bind(this, i));
+        // DicsLists.push(NaN);
+
+        DicsItems.push(document.querySelector(".b-dics" + i.toString()));
+        DicsLists.push(new Dics({
+            container: DicsItems[i],
+            textPosition: "top"
+        }));
+        DicsItems[i].classList.add("hide");
     }
+    _change_active_comp_dot(0);
 });
 
 
@@ -31,9 +43,12 @@ function _change_active_dot (idx) {
     carousel_item_idx = idx;
 }
 function _change_active_comp_dot (idx) {
-    CarouselCompDots[idx].classList.add("active");
+    console.log("id", idx);
+    CarouselCompDots[idx].classList.add("active"); // change button
+    DicsItems[idx].classList.remove("hide"); // change comparison
     if (carousel_comp_item_idx != idx) {
         CarouselCompDots[carousel_comp_item_idx].classList.remove("active");
+        DicsItems[carousel_comp_item_idx].classList.add("hide");
     }
     carousel_comp_item_idx = idx;
 }
